049627e180c53a21585e37e4d83cd947164eed91
AlbaraaAlpaka <105862881+AlbaraaAlpaka@users.noreply.github.com>
2024-11-06 14:47:32 +0100

    finished with put-methode

--- solver.hs
+++ solver.hs
@@ -1,8 +1,8 @@
 type Sudoku = [[Int]]
 
 -- Ein eindeutig lösbares Sudoku-Rätsel
-sudoku :: Sudoku
-sudoku =
+sudokuExample :: Sudoku
+sudokuExample =
 --  c1 c2
   [ [5, 3, 0, 0, 7, 0, 0, 0, 0],    --r1
     [6, 0, 0, 1, 9, 5, 0, 0, 0],    --r2
@@ -15,14 +15,21 @@ sudoku =
     [0, 0, 0, 0, 8, 0, 0, 7, 9]
   ]
 
---          Array    col    row    num
-isValid :: Sudoku -> Int -> Int -> Int -> Bool
-isValid s c r n =
-  let x = put s c r n
-   in validColum x && validRow x && validBox x
+-- --          Array    col    row    num
+-- isValid :: Sudoku -> Int -> Int -> Int -> Bool
+-- isValid s c r n =
+--   let x = put s c r n
+--    in validColum x && validRow x && validBox x
 
 --     Array     col    row    num    ArrayResult
 put :: Sudoku -> Int -> Int -> Int -> Sudoku
-put s c r n =   take r s ++
-                [replaceIndex (drop r (take (r+1) s)) c n] ++   -- (drop (r-1)(take r s)) = (s !! r)
-                drop (r+1) s
+put s col row num =   take row s ++
+                [replaceIndex (s !! row) col num] ++   -- (drop (r-1)(take r s)) = (s !! r)
+                drop (row+1) s
+
+replaceIndex :: [Int] -> Int -> Int -> [Int]
+replaceIndex s c n =    take c s ++
+                        [n] ++
+                        drop (c+1) s
+
+
