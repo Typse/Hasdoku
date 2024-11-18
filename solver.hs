8d0b4e235b48a39b597de25796a5e6cfd2c31927
AlbaraaAlpaka <105862881+AlbaraaAlpaka@users.noreply.github.com>
2024-11-18 20:49:12 +0100

    solver finished

--- solver.hs
+++ solver.hs
@@ -1,6 +1,6 @@
+import Data.List
 import System.Console.Terminfo (Point (col))
 import System.FilePath (isValid)
-import Data.List
 
 type Sudoku = [[Int]]
 
@@ -21,34 +21,34 @@ sudokuExample =
 
 --             Array    col    row    num
 validInput :: Sudoku -> Int -> Int -> Int -> Bool
-validInput s c r n =
-  let x = put s c r n
-   in validColumn x c && validRow x r && validBox x c r
+validInput sudoku column row num =
+  let x = put sudoku column row num
+   in validColumn x column && validRow x row && validBox x column row
 
 -- Fügt eine Zahl in ein Sudoku Feld ein
 --     Array     col    row    num    ArrayResult
 put :: Sudoku -> Int -> Int -> Int -> Sudoku
-put s col row num =
-  take row s
-    ++ [replaceIndex (s !! row) col num]
-    ++ drop (row + 1) s
+put sudoku col row num =
+  take row sudoku
+    ++ [replaceIndex (sudoku !! row) col num]
+    ++ drop (row + 1) sudoku
 
 -- Hilfsfunktion
 replaceIndex :: [Int] -> Int -> Int -> [Int]
-replaceIndex s c n =
-  take c s
-    ++ [n]
-    ++ drop (c + 1) s
+replaceIndex sudoku column num =
+  take column sudoku
+    ++ [num]
+    ++ drop (column + 1) sudoku
 
 -- Valide Box
 validBox :: Sudoku -> Int -> Int -> Bool
-validBox s col row = validLine (getBox s row col)
+validBox sudoku col row = validLine (getBox sudoku row col)
 
 getBox :: Sudoku -> Int -> Int -> [Int]
-getBox s row col =
-  [ s !! r !! c
-  | r <- [boxRowStart .. boxRowStart + 2],
-    c <- [boxColStart .. boxColStart + 2]
+getBox sudoku row col =
+  [ sudoku !! r !! c
+    | r <- [boxRowStart .. boxRowStart + 2],
+      c <- [boxColStart .. boxColStart + 2]
   ]
   where
     boxRowStart = (row `div` 3) * 3
@@ -56,18 +56,47 @@ getBox s row col =
 
 -- Valide Spalte
 validColumn :: Sudoku -> Int -> Bool
-validColumn s col = validLine(getColumn s col)
+validColumn sudoku col = validLine (getColumn sudoku col)
 
 validRow :: Sudoku -> Int -> Bool
-validRow s row = validLine(s !! row)
+validRow sudoku row = validLine (sudoku !! row)
 
 -- Hilffunktion: gibt die Column rückwärts!!!
 getColumn :: Sudoku -> Int -> [Int]
-getColumn s col = map (\row -> row !! col) s
+getColumn sudoku col = map (\row -> row !! col) sudoku
 
 -- Überprüft ein Array auf valide, nub filter alle duplikate, length gibt die länge aus
 validLine :: [Int] -> Bool
 validLine xs =
   let nonZero = filter (/= 0) xs
-  in length nonZero == length (nub nonZero)
+   in length nonZero == length (nub nonZero)
 
+-- Feld    column  row   Zahl in dem Feld
+getIndex :: Sudoku -> Int -> Int -> Int
+getIndex sudoku column row = getColumn sudoku column !! row
+
+-- Jetzt kommt der eigentliche Solver
+solve :: Sudoku -> (Sudoku, Bool)
+solve sudoku = solveSudokuHelper sudoku 0 0
+
+--                    Feld      column  row   gelöstes Feld
+solveSudokuHelper :: Sudoku -> Int -> Int -> (Sudoku, Bool)
+solveSudokuHelper sudoku column row
+  | row == 9 = (sudoku, True)
+  | column == 9 = solveSudokuHelper sudoku 0 (row + 1)
+  | getIndex sudoku column row /= 0 = solveSudokuHelper sudoku (column + 1) row
+  | otherwise =
+      let (sudoku', success) = solveSudokuHelperForloop sudoku column row 1
+       in if success then (sudoku', True) else (sudoku, False)
+
+-- For loop for trying out nums from 1 to 9
+--                          Feld      column  row    Num    Feld
+solveSudokuHelperForloop :: Sudoku -> Int -> Int -> Int -> (Sudoku, Bool)
+solveSudokuHelperForloop sudoku column row num
+  | num == 10 = (sudoku, False)
+  | validInput sudoku column row num =
+      let (next_sudoku, success) = solveSudokuHelper (put sudoku column row num) (column + 1) row
+       in if success
+            then (next_sudoku, True)
+            else solveSudokuHelperForloop sudoku column row (num + 1)
+  | otherwise = solveSudokuHelperForloop sudoku column row (num + 1)
